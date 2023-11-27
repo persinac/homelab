5f5cba580ce680829d0b9eeda2a2d2d41a2c523d
Alex Persinger <apfbacc@gmail.com>
2023-11-26 22:41:38 -0500

    namespace creation worked, trying out cluster issuer

--- test-k8s-provision/__main__.py
+++ test-k8s-provision/__main__.py
@@ -1,5 +1,11 @@
 import pulumi
 import pulumi_rke as rke
+from pulumi_kubernetes import Provider
+from pulumi_kubernetes.apps.v1 import Deployment
+from pulumi_kubernetes.core.v1 import Namespace, Service, Secret
+from pulumi_kubernetes.apiextensions.v1 import CustomResourceDefinition
+from pulumi_kubernetes.apiextensions import CustomResource
+from pulumi_kubernetes.yaml import ConfigFile
 
 with open('C:\\homelab\\id_rsa', 'r') as file:
     ssh_key = file.read()
@@ -25,3 +31,147 @@ cluster = rke.Cluster(
 kubeconf = cluster.kube_config_yaml
 print(kubeconf)
 pulumi.export("kubeconfig", kubeconf)
+
+# set up provider
+k8s_provider = Provider("k8s-provider", kubeconfig=kubeconf)
+
+# Set up cert-manager CRDs
+# crds = ConfigFile("cert-manager-crds",
+#                   file="https://raw.githubusercontent.com/jetstack/cert-manager/release-0.11/deploy/manifests/00-crds.yaml",
+#                   opts=pulumi.ResourceOptions(provider=k8s_provider))
+
+
+# setup cert-manager
+namespace = Namespace(
+    'cert-manager',
+    metadata={
+        "name": "cert-manager",
+        "labels": {
+            "certmanager.k8s.io/disable-validation": "true"
+        }
+    },
+    opts=pulumi.ResourceOptions(provider=k8s_provider),
+)
+
+# cert-manager clusterIssuer
+cluster_issuer = CustomResource(
+    "letsencrypt-staging",
+    api_version="cert-manager.io/v1",
+    kind="Issuer",
+    metadata={
+        "name": "letsencrypt-staging",
+        "namespace": namespace.metadata["name"]
+    },
+    spec={
+        "acme": {
+            "email": "apfbacc@gmail.com",
+            "server": "https://acme-v02.api.letsencrypt.org/directory",
+            "privateKeySecretRef": {
+                "name": "letsencrypt-staging"
+            },
+            "solvers": [{
+                "http01": {
+                    "ingress": {
+                        "class": "nginx"
+                    }
+                }
+            }]
+        }
+    },
+    opts=pulumi.ResourceOptions(provider=k8s_provider),
+)
+#
+# # Create a TLS certificate
+# cert = CustomResource(
+#     "tls-certificate",
+#     api_version="cert-manager.io/v1",
+#     kind="Certificate",
+#     metadata={"name": "test-certificate", "namespace": "dev"},
+#     spec={
+#         "secretName": "test-certificate",
+#         "dnsNames": ["rhino-augmented.ts.net"],
+#         "acme": {
+#             "config": [
+#                 {
+#                     "http01": {
+#                         "ingressClass": "nginx"
+#                     },
+#                     "domains": ["rhino-augmented.ts.net"]
+#                 }
+#             ]
+#         },
+#         "issuerRef": {
+#             "name": "letsencrypt-staging",
+#             "kind": "ClusterIssuer"
+#         }
+#     },
+#     opts=pulumi.ResourceOptions(provider=k8s_provider, depends_on=[cluster_issuer]))
+#
+# # Deploy Nginx Ingress
+# ingress = Deployment(
+#     "nginx-ingress",
+#     metadata={
+#         "namespace": "dev",
+#         "name": "demo"
+#     },
+#     spec={
+#         "replicas": 1,
+#         "selector": {
+#             "matchLabels": {
+#                 "app.kubernetes.io/name": "demo",
+#                 "app.kubernetes.io/part-of": "demo"
+#             }
+#         },
+#         "template": {
+#             "metadata": {
+#                 "labels": {
+#                     "app.kubernetes.io/name": "demo",
+#                     "app.kubernetes.io/part-of": "demo"
+#                 },
+#                 "annotations": {
+#                     "nginx.ingress.kubernetes.io/ssl-redirect": "true"  # Redirects http to https.
+#                 }
+#             },
+#             "spec": {
+#                 "containers": [{
+#                     "name": "nginx",
+#                     "image": "nginx:1.14.2",
+#                     "ports": [
+#                         {"name": "http", "containerPort": 80},
+#                         {"name": "https", "containerPort": 443}
+#                     ],
+#                     "volumeMounts": [{
+#                         "mountPath": "/etc/nginx/ssl",
+#                         "name": "tls-certificate"
+#                     }]
+#                 }],
+#                 "volumes": [{
+#                     "name": "tls-certificate",
+#                     "secret": {
+#                         "secretName": cert.spec.apply(lambda spec: spec["secretName"])
+#                     },
+#                 }]
+#             }
+#         }
+#     },
+#     opts=pulumi.ResourceOptions(provider=k8s_provider, depends_on=[cert])
+# )
+#
+# ingress_service = Service(
+#     "demo-service",
+#     metadata={
+#         "namespace": "dev",
+#         "name": "demo"
+#     },
+#     spec={
+#         "selector": ingress.spec["template"]["metadata"]["labels"],
+#         "ports": [
+#             {"name": "http", "port": 80, "targetPort": 80},
+#             {"name": "https", "port": 443, "targetPort": 443}
+#         ],
+#         "type": "LoadBalancer"
+#     },
+#     opts=pulumi.ResourceOptions(provider=k8s_provider, depends_on=[ingress])
+# )
+
+# pulumi.export("ingress_ip", ingress_service.status.apply(lambda s: s['loadBalancer']['ingress'][0]['ip']))
