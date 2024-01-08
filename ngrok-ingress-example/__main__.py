01c518a4d1cb751e23bc6bfb7f8275b3d21d5ed6
Alex Persinger <apfbacc@gmail.com>
2024-01-07 23:52:34 -0500

    Updated ngrok ingress example to use the ngrok up-to-date kubernetes controller instead of the old version.

--- ngrok-ingress-example/__main__.py
+++ ngrok-ingress-example/__main__.py
@@ -37,60 +37,6 @@ ngrok_auth_token = os.getenv('ngrok_auth_token')
 
 namespace_ngrok_ingress_controller_name = 'ngrok-ingress-controller-example'
 
-# CRDs
-with open('manifest_yamls/custom-resource-definitions.yaml', 'r') as file:
-    templated_yaml_content = file.read()
-
-# Inject .env values and get formatted yaml
-injected_yaml_content = replace_placeholders(
-    templated_yaml_content,
-    {
-        'ngrok_domain_in': ngrok_subdomain,
-        'ngrok-namespace': namespace_ngrok_ingress_controller_name
-    }
-)
-
-injected_crds_yaml_file_name = 'formatted_yamls/custom-resource-definitions.yaml'
-with open(injected_crds_yaml_file_name, 'w') as file:
-    file.write(injected_yaml_content)
-
-
-# Cluster Roles
-with open('manifest_yamls/cluster-roles.yaml', 'r') as file:
-    templated_yaml_content = file.read()
-
-# Inject .env values and get formatted yaml
-injected_yaml_content = replace_placeholders(
-    templated_yaml_content,
-    {
-        'ngrok_domain_in': ngrok_subdomain,
-        'ngrok-namespace': namespace_ngrok_ingress_controller_name
-    }
-)
-
-injected_cr_yaml_file_name = 'formatted_yamls/cluster-roles.yaml'
-with open(injected_cr_yaml_file_name, 'w') as file:
-    file.write(injected_yaml_content)
-
-
-# Cluster Role Bindings
-with open('manifest_yamls/cluster-role-bindings.yaml', 'r') as file:
-    templated_yaml_content = file.read()
-
-# Inject .env values and get formatted yaml
-injected_yaml_content = replace_placeholders(
-    templated_yaml_content,
-    {
-        'ngrok_domain_in': ngrok_subdomain,
-        'ngrok-namespace': namespace_ngrok_ingress_controller_name
-    }
-)
-
-injected_crbs_yaml_file_name = 'formatted_yamls/cluster-role-bindings.yaml'
-with open(injected_crbs_yaml_file_name, 'w') as file:
-    file.write(injected_yaml_content)
-
-
 # Game deployment
 with open('manifest_yamls/ingress-game-deployment.yaml', 'r') as file:
     templated_yaml_content = file.read()
@@ -104,13 +50,12 @@ injected_yaml_content = replace_placeholders(
     }
 )
 
-injected_game_deploy_yaml_file_name = 'formatted_yamls/ingress-game-deployment.yaml'
+injected_game_deploy_yaml_file_name = 'formatted_yamls/test-this.yaml'
 with open(injected_game_deploy_yaml_file_name, 'w') as file:
     file.write(injected_yaml_content)
 
-
 # Define k8s stack reference
-stack_ref = pulumi.StackReference("persinac/k8s-provision/dev-k8s-stack")
+stack_ref = pulumi.StackReference("persinac/k8s-provision/dev")
 kubeconfig = stack_ref.get_output("kubeconfig")
 print(kubeconfig)
 
@@ -134,42 +79,12 @@ namespace_ngrok_ingress_controller = Namespace(
 
 ## https://raw.githubusercontent.com/ngrok/kubernetes-ingress-controller/main/manifest-bundle.yaml
 
-# Apply the templated YAML content as a configfile
-ngrok_game_manifest_crds = ConfigFile(
-    'ngrok-game-manifest-crds',
-    file=injected_crds_yaml_file_name,
-    opts=pulumi.ResourceOptions(
-        provider=k8s_provider,
-        depends_on=[namespace_ngrok_ingress_controller]
-    )
-)
-
-# Apply the templated YAML content as a configfile
-ngrok_game_manifest_cr = ConfigFile(
-    'ngrok-game-manifest-cr',
-    file=injected_cr_yaml_file_name,
-    opts=pulumi.ResourceOptions(
-        provider=k8s_provider,
-        depends_on=[ngrok_game_manifest_crds]
-    )
-)
-
-# Apply the templated YAML content as a configfile
-ngrok_game_manifest_crbs = ConfigFile(
-    'ngrok-game-manifest-crbs',
-    file=injected_crbs_yaml_file_name,
-    opts=pulumi.ResourceOptions(
-        provider=k8s_provider,
-        depends_on=[ngrok_game_manifest_cr]
-    )
-)
-
 
 # define the helm chart
 ingress_controller_chart = Chart(
-    "ngrok-ingress-controller",
+    "kubernetes-ingress-controller",
     ChartOpts(
-        chart="ngrok-ingress-controller",
+        chart="kubernetes-ingress-controller",
         fetch_opts=FetchOpts(
             repo="https://ngrok.github.io/kubernetes-ingress-controller"
         ),
@@ -185,9 +100,7 @@ ingress_controller_chart = Chart(
     opts=pulumi.ResourceOptions(
         provider=k8s_provider,
         depends_on=[
-            ngrok_game_manifest_crds,
-            ngrok_game_manifest_cr,
-            ngrok_game_manifest_crbs
+            namespace_ngrok_ingress_controller
         ]
     )
 )
